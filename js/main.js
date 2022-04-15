939c56a1ec30382da22ffdc1fe324368a40404a7
Ferreira <contosdeferreira@hotmail.com>
2022-04-14 22:52:55 -0300

    concluindo site

--- js/main.js
+++ js/main.js
@@ -1,42 +1,22 @@
 const nome = document.getElementById('name');
 const celular = document.getElementById('celular');
 const form = document.getElementById('form');
-const convidados = JSON.parse(localStorage.getItem("convidados")) || []
-const listaDeConvidados = convidados || []
+ let nomeAtual = JSON.parse(localStorage.getItem("nomeAtual")) || ''
+while (nomeAtual === '') {
+    nomeAtual = prompt('Digite seu primeiro nome')
+}
 
-convidados.forEach( (elemento) => {
-    console.log(elemento.nome, elemento.celular)
-})
+const paragrafo = document.querySelector(".cabeçalho__paragrafo")
+
+paragrafo.textContent = `${nomeAtual}, ficaremos muito felizes de ter você na celebração do nosso casamento. Queremos muito que participe do momento mais marcante de nossas vidas. Esperamos te ver lá.`
 
 form.addEventListener('submit', (e) => {
-    let messages = []
-    url = "localhost:3000"
-
-    if (isNaN(celular.value)){
-        messages.push('Número inválido demais')
-        alert('O número do seu celular não pode conter letras')
-    } else if (celular.value.length != 11) {
-        messages.push('Número inválido')
-        alert('Número inválido! Não esqueça do DDD')
-    } 
-
-    if (messages.length > 0) {        
-        e.preventDefault()
-    } 
-    
-    else {
         e.preventDefault()
 
-        const convidadoAtual = {
-            "nome": nome.value,
-            "celular": celular.value
-        }
+        alert('Presença confirmada!')
 
-        listaDeConvidados.push(convidadoAtual)
-
-        localStorage.setItem("convidados", JSON.stringify(listaDeConvidados))
+        localStorage.setItem("nomeAtual", JSON.stringify(nomeAtual))
 
         nome.value = ""
         celular.value = ""
-    }
 })
