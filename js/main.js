bd08b3a278b39e968e646424a8a5a9b0e14888fc
Felipe <ferreira.contato1@gmail.com>
2022-05-09 09:48:37 -0300

    Adicionado popup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+const fnome = document.getElementById('fname');
 const nome = document.getElementById('name');
 const celular = document.getElementById('celular');
 const form = document.getElementById('form');
- let nomeAtual = JSON.parse(localStorage.getItem("nomeAtual")) || ''
-while (nomeAtual === '') {
-    nomeAtual = prompt('Digite seu primeiro nome')
+const form2 = document.getElementById('form2');
+
+let nomeAtual = JSON.parse(localStorage.getItem("nomeAtual")) || ''
+if (nomeAtual == '') {
+    document.getElementById('modal-popup').style.display = 'flex';
 }
 
 const paragrafo = document.querySelector(".cabeçalho__paragrafo")
@@ -11,12 +14,23 @@ const paragrafo = document.querySelector(".cabeçalho__paragrafo")
 paragrafo.textContent = `${nomeAtual}, ficaremos muito felizes de ter você na celebração do nosso casamento. Queremos muito que participe do momento mais marcante de nossas vidas. Esperamos te ver lá.`
 
 form.addEventListener('submit', (e) => {
-        e.preventDefault()
+    e.preventDefault()
 
-        alert('Presença confirmada!')
+    alert('Presença confirmada!')
 
+    nomeAtual = nome.value;
+    localStorage.setItem("nomeAtual", JSON.stringify(nomeAtual))
+
+    nome.value = ""
+})
+
+
+form2.addEventListener('submit', (e) => {
+
+    if (fnome.value != '') {
+        nomeAtual = fnome.value
         localStorage.setItem("nomeAtual", JSON.stringify(nomeAtual))
+        document.getElementById('modal-popup').style.display = 'none';
+    }
 
-        nome.value = ""
-        celular.value = ""
 })
